c0fa5f849859fff19c77f658c6ccf4e3ecafa9cb
Samura <shortwing@list.ru>
2022-08-04 13:19:00 +0300

    Перписал main для удобного использования

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:project/pages/profile.dart';
-import 'package:project/pages/map.dart';
+import 'package:project/pages/mainscreen.dart';
+
+void main() => runApp(MyApp());
 
 
-void main() => runApp( MaterialApp(
-  initialRoute: '/',
-  routes: {
+class MyApp extends StatelessWidget {
 
-    '/': (context) => Myapp(),
-    '/map': (context) => BackgroundMap(),
-  },
-));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     theme: ThemeData(
+       bottomNavigationBarTheme: BottomNavigationBarThemeData(
+         backgroundColor: Colors.indigo, //тут почему то не хочет отображать цвет панели
+         unselectedItemColor: Colors.black,//тут невыбранные элементы отображаются цветом
+         selectedItemColor: Colors.green,//цвет выбранного элемента
+       ),
+     ),
+      initialRoute: '/',
+      routes: {
+       '/': (context) => MainScreen(),
+      },
+    );
+  }
+}
+
+
+
 
 
 
